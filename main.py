87270a0c264a1e459f40087c5dec6aa889656475
Jonson <Jonson@gmailc.om>
2017-08-28 17:51:44 +0800

    stock class

--- main.py
+++ main.py
@@ -17,37 +17,40 @@ l_low = []
 l_close = []
 #l_volume = []
 
-f_UpDown = [] #當日的漲跌量
-#f_UpDownMax = [] #當日最大的漲跌量
+f_UpDown = []  # 當日的漲跌量
+# f_UpDownMax = [] #當日最大的漲跌量
 
-def moving_average(a, n=5) :
+
+def moving_average(a, n=5):
     ret = np.cumsum(a, dtype=float)
     ret[n:] = ret[n:] - ret[:-n]
-    #return ret[n - 1:] / n
+    # return ret[n - 1:] / n
     return ret[:] / n
 
-def BIAS(a, n=5) :
+
+def BIAS(a, n=5):
     ret = np.cumsum(a, dtype=float)
     ret[n:] = ret[n:] - ret[:-n]
-    ret = ret/n #average
-    ret =(a/ ret);
-    ret = (1-ret)*100
+    ret = ret / n  # average
+    ret = (a / ret)
+    ret = (1 - ret) * 100
     return ret[:]
+
 
 for index, data in enumerate(my_data):
     tmp_open = data[1]
     tmp_high = data[2]
     tmp_low = data[3]
     tmp_close = data[4]
-    
+
     l_open.append(tmp_open)
     l_high.append(tmp_high)
     l_low.append(tmp_low)
     l_close.append(tmp_close)
-    
+
     f_UpDown.append(tmp_close - tmp_open)
 
-    
+
 f_ma5 = moving_average(l_close, n=5)
 f_ma10 = moving_average(l_close, n=10)
 #f_ma20 = moving_average(l_close, n=20)
@@ -63,40 +66,35 @@ f_BIAS30 = BIAS(l_close, n=30)
 f_BIAS40 = BIAS(l_close, n=40)
 f_BIAS50 = BIAS(l_close, n=50)
 f_BIAS60 = BIAS(l_close, n=60)
-    
+
+score = -99
+for nday in range(1, 60):  # n天後的漲跌
+    for l in range(1, 60):
+        feature = BIAS(l_close, l)
+        y = np.array(l_close)  # np.array 才能直接運算
+        y[:-nday] = y[nday:] - y[:-nday]
+        x = np.stack(zip(feature))  # todo 需要多個feature
+
+        model = LinearRegression()
+        tmp_size = len(x) - 100 - nday
+        model.fit(x[60:tmp_size], y[60:tmp_size])
+        y1 = model.predict(x[tmp_size:])
+        tmp_score = model.score(x[tmp_size:], y[tmp_size:])
+        if(tmp_score > score):
+            score = tmp_score
+            print(nday, l, score)
 
 
-y = np.array(l_close) #np.array 才能直接運算
-nday = 10 #n天後的漲跌
-y[:-nday] = y[nday:] - y[:-nday]   
-x = np.stack(zip(f_BIAS5)) #todo 需要多個feature
-
-
-model = LinearRegression()
-tmp_size = x.size-100-nday
-model.fit(x[60:tmp_size],y[60:tmp_size])
-y1 = model.predict(x[tmp_size:])
-score = model.score(x[tmp_size:],y[tmp_size:])
-print(score)
-
-
-#畫圖
+# 畫圖
 from sklearn.cross_validation import cross_val_predict
 
-x= x[60:tmp_size]
-y= y[60:tmp_size]
-predicted = cross_val_predict(model,x,y, cv=10)
+x = x[60:tmp_size]
+y = y[60:tmp_size]
+predicted = cross_val_predict(model, x, y, cv=10)
 fig, ax = plt.subplots()
 ax.scatter(y, predicted)
 ax.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw=4)
 ax.set_xlabel('Measured')
 ax.set_ylabel('Predicted')
 plt.show()
-
-#plt.plot(l_close)
-#plt.plot(x)
-#plt.show()
-
-
-
 
---
+++ stock.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+
+class stock:
+    def __init__(self, filename):
+        self.raw = np.genfromtxt(
+            filename,
+            delimiter=',',
+            skip_header=1)
+        self.raw = self.raw[::-1]  # 反向
+
+        self.opened = []
+        self.high = []
+        self.low = []
+        self.close = []
+
+        for index, data in enumerate(self.raw):
+            tmp_open = data[1]
+            tmp_high = data[2]
+            tmp_low = data[3]
+            tmp_close = data[4]
+            self.opened.append(tmp_open)
+            self.high.append(tmp_high)
+            self.low.append(tmp_low)
+            self.close.append(tmp_close)
+            #f_UpDown.append(tmp_close - tmp_open)
+
+    def feature_MA(self, n=5):  # 移動平均線
+        ret = np.cumsum(self.close, dtype=float)
+        ret[n:] = ret[n:] - ret[:-n]
+        # return ret[n - 1:] / n
+        return ret[:] / n
+
+    def feature_BIAS(self, n=5):  # 乖離率
+        ret = np.cumsum(self.close, dtype=float)
+        ret[n:] = ret[n:] - ret[:-n]
+        ret = ret / n  # average
+        ret = (self.close / ret)
+        ret = (1 - ret) * 100
+        return ret[:]
+
+    def feature_RSV(self, n=9):
+        high_n = []
+        low_n = []
+        result = []
+        for i in range(0, len(self.high)):
+            start = i - n + 1
+            end = i + 1
+            if(start < 0):
+                start = 0
+
+            high_n.append(max(self.high[start:end]))
+            low_n.append(min(self.low[start:end]))
+
+        
+        h = np.array(high_n)
+        l = np.array(low_n)
+        c = np.array(self.close)
+        result = ((c - l) / (h - l)) * 100
+       
+        print(((c[2457] - l[2457]) / (h[2457] - l[2457])) * 100)
+
+        return (result)
+
+    def feature_K(self, n=9):
+        K = []
+        RSV = self.feature_RSV(n)
+        for i in range(0, len(self.high)):
+            pre_K = 50
+            if(i > 0):
+                pre_K = K[i - 1]
+            current_K = pre_K * (2 / 3) + RSV[i] * (1 / 3)
+            K.append(current_K)
+        return K
+
+    def feature_D(self, n=9):
+        D = []
+        K = self.feature_K(n)
+        for i in range(0, len(self.high)):
+            pre_D = 50
+            if(i > 0):
+                pre_D = D[i - 1]
+            current_D = pre_D * (2 / 3) + K[i] * (1 / 3)
+            D.append(current_D)
+        return D
+
+
+a = stock("price_data.csv")
+#b = a.feature_K()
+RSV = a.feature_RSV()
